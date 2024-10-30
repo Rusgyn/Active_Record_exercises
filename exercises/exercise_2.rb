0ea388c5220c13a46a3f16da266953605d007101
rusgyn <rusgynmagsanay@gmail.com>
2024-10-30 17:31:33 -0400

    Code that generate find a store and update the store.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.update(name: "Update_Burnaby")
+@store1.save
+
+p @store1.name # => "Update_Burnaby"
+p @store2.name # => "Richmond"
